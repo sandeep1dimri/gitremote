ef7ba3715af9eb4797f49b9f3dee191c3cfbd74c
Sandeep <Sandeep1dimri@gmail.com>
2020-06-07 23:25:55 -0400

    added cat sound

--- animal.py
+++ animal.py
@@ -1,10 +1,16 @@
 import sys
 
+def cat():
+	print("meow meow!!")
+
 def default():
 	print("No Animal Sound")
 
 def main():
-	default()
+	if sys.argv[1]=="cat":
+		cat()
+	else:
+		default()
 
 if __name__=="__main__":
 	main()
